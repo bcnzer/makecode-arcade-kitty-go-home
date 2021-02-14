939e97f3a327ae66c865ec42239257ae3ccf8ec7
Ben Chartrand <bcnzer@users.noreply.github.com>
2021-02-14 16:26:39 +1300

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Open this page at [https://bcnzer.github.io/makecode-arcade-kitty-go-home/](https://bcnzer.github.io/makecode-arcade-kitty-go-home/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/bcnzer/makecode-arcade-kitty-go-home** and import
+
+## Edit this project ![Build status badge](https://github.com/bcnzer/makecode-arcade-kitty-go-home/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/bcnzer/makecode-arcade-kitty-go-home** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/bcnzer/makecode-arcade-kitty-go-home/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ tilemap.g.ts
@@ -0,0 +1,55 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100002030201080302020102030202010214020204010206020301020202020102150203020102090b02010502040201021503020501020b090201020202020105150202020102090b04010203060201021502030701090a0b0201020c0d0e0102150406020102020503010211100f0103150302020102020206010211100f0102150208020102060405010312101301081502030201020b03070102080605010215020202010b0b02080102020909010415020406010b0b0302010502090901021502030201050b0402010203090901031502020201020b020201030309090108150202040103020b02010202060901021502020301020202030102020206010216`, img`
+. . . . 2 . . . . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . . . 2 2 . . 2 . . . . . . 
+. . 2 . . 2 2 . . . . . . . 2 . 
+. . . . . 2 2 . . . . 2 . . . . 
+. . 2 . 2 2 2 . . . 2 2 2 . . . 
+. 2 . . . . 2 . . . 2 2 2 . . . 
+. . . . . . . 2 . . 2 2 2 . . . 
+. 2 . . . 2 . 2 . . 2 2 2 . 2 . 
+. . . . . 2 . 2 . . 2 2 2 . . . 
+. . . . 2 2 . 2 . . . 2 2 . . . 
+. . 2 . 2 2 . . . 2 . 2 2 . . . 
+. . . . 2 2 . . . . . 2 2 . . . 
+. . . . . 2 . . . . . 2 2 . 2 . 
+. . . . . . 2 . . . . 2 2 . . . 
+. . . . . . . . . . . . 2 . . . 
+`, [myTiles.transparency16,sprites.vehicle.roadVertical,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.castle.tileGrass2,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile5,sprites.castle.tileDarkGrass3,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass1,sprites.builtin.forestTiles5,sprites.builtin.forestTiles6,sprites.builtin.forestTiles7,sprites.builtin.forestTiles11,sprites.builtin.forestTiles10,sprites.builtin.forestTiles9,sprites.builtin.forestTiles13,sprites.builtin.forestTiles15,sprites.castle.tilePath1,sprites.castle.tilePath4,sprites.castle.tilePath7], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTreee":
+            case "tile2":return tile2;
+            case "myTree2":
+            case "tile3":return tile3;
+            case "myTree3":
+            case "tile5":return tile5;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
